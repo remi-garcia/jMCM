b133e5fbe1f3aa8f83e55655859d0329ec67e512
Rémi <47191601+remi-garcia@users.noreply.github.com>
2024-04-18 11:47:12 +0200

    Write addergraph and print info

--- jMCM.jl
+++ jMCM.jl
@@ -37,7 +37,8 @@ function read_args(args)
             elseif current_kw == "wIn"
                 all_args.wIn = parse(Int, current_value)
             elseif current_kw == "wOut"
-                all_args.wOut = parse(Int, current_value)
+                println("Keyword `$current_kw` not yet implemented")
+                #all_args.wOut = parse(Int, current_value)
             elseif current_kw == "pipeline"
                 all_args.pipeline = parse(Bool, current_value)
             elseif current_kw == "verbose"
@@ -54,6 +55,8 @@ function read_args(args)
                 all_args.threads = parse(Int, current_value)
             elseif current_kw == "ws_timelimit"
                 all_args.ws_timelimit = parse(Float64, current_value)
+            elseif current_kw == "addergraph_file"
+                all_args.file_ag = current_value
             else
                 println("Unrecognized keyword `$current_kw` ignored")
             end
@@ -87,6 +90,17 @@ function main(args)
              nb_adders_start=all_args.nb_adders_start,
              use_warmstart=all_args.use_rpag
     )
+
+    println("Adder Graph obtained for constants $(all_args.target_consts)")
+    println("Cost in terms of adders: $(length(get_nodes(ag)))")
+    all_args.wIn > 0 && println("Cost in terms of one-bit adders: $(compute_total_nb_onebit_adders(ag, all_args.wIn))")
+
+    open("$(all_args.file_ag)", "w") do writefile
+        write(writefile, "$(write_addergraph(ag, pipeline=all_args.pipeline))\n")
+        if all_args.wOut > 0
+            write(writefile, "$(write_addergraph_truncations(ag))\n")
+        end
+    end
 
     return nothing
 end
